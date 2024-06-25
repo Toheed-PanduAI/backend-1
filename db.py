39369868d9773bb9b3d678ea851ae0f7353b4281
Toheed-PanduAI <toheed.jamaal@pandu.ai>
2024-06-25 12:31:32 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,7 +2,8 @@ import pymongo
 import os
 
 # MONGO_URL = os.getenv("MONGO_DB_URL", "mongodb://localhost:27017")
-MONGO_URL = "mongodb://localhost:27017" # for local development
+# MONGO_URL = "mongodb://localhost:27017" # for local development
+MONGO_URL = "mongodb+srv://toheed:toheed123@cluster0.by2qgls.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 myclient = pymongo.MongoClient(MONGO_URL)
 db = myclient["panduAI_db"]
 users_collection = db["users"]
